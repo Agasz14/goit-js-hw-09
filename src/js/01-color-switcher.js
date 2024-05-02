ad0fe513fd23a307a68cb931357f576b3239c57e
Agasz14 <146726350+Agasz14@users.noreply.github.com>
2024-05-02 16:19:42 +0200

    zadanie

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+// funkcja do zmiany koloru ustawiona na co jedną sekundę
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();  
+    setTimeout(changeBackgroundColor, 1000);
+}
+
+const buttonStart = document.querySelectorAll('button[data-start]');
+const buttonStop = document.querySelectorAll('button[data-stop]');
+
+// funkcjonalność przycisku Start
+buttonStart.addEventListener('click', function(changeBackgroundColor) {
+    this.disabled = true; //po kliknięciu przycisk Start zostaje wyłączon
+    buttonStop.disabled = false; // przycisk Stop zostaje uaktywoniony
+    changeBackgroundColor(); // włączenie funkcji zmiany koloru tła
+})
+
+// funkcjonalność przycisku Stop
+buttonStop.addEventListener('click', function() {
+    this.disabled = true; //po kliknięciu przycisk Stop zostaje wyłączony
+    buttonStart.disabled = false; // przycisk Start zostaje uaktywoniony
+    return (changeBackgroundColor).reject ; // wyłączenie funkcji na zmianę koloru
+})
 
